d41142fbf613060c03db0e3ba5699f3f24ba5e69
Chris Thoburn <runspired@gmail.com>
2016-06-28 14:47:38 -0400

    feat(timers): stubs a future timer interface

---
+++ addon/-private/backburner/index.js
@@ -0,0 +1,19 @@
+export default class Backburner {
+
+  constructor() {
+
+  }
+
+  throttle() {
+    throw new Error('timers are not implemented yet!');
+  }
+
+  debounce() {
+    throw new Error('timers are not implemented yet!');
+  }
+
+  later() {
+    throw new Error('timers are not implemented yet!');
+  }
+
+}
