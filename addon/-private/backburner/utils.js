70265042b9ebfc2fcb463c484d3a2b2ea985ad3d
Chris Thoburn <runspired@gmail.com>
2016-07-12 18:40:19 -0500

    feat(backburner): ports utils from backburner.js and adds isNullOrUndefined utility

---
+++ addon/-private/backburner/utils.js
@@ -0,0 +1,27 @@
+const NUMBER = /\d+/;
+
+export function each(collection, callback) {
+  for (var i = 0; i < collection.length; i++) {
+    callback(collection[i]);
+  }
+}
+
+export function isNullOrUndefined(suspect) {
+  return suspect === undefined || suspect === null;
+}
+
+export function isString(suspect) {
+  return typeof suspect === 'string';
+}
+
+export function isFunction(suspect) {
+  return typeof suspect === 'function';
+}
+
+export function isNumber(suspect) {
+  return typeof suspect === 'number';
+}
+
+export function isCoercableNumber(number) {
+  return isNumber(number) || NUMBER.test(number);
+}
